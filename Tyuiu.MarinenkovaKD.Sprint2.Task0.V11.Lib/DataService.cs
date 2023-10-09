b8be5e061b74661fffe434207b10d10f22300b28
Tyuiu.MarinenkovaKD@outlook.com <marinenkova05@gmail.com>
2023-10-09 21:18:55 +0500

    Task0

---
+++ Tyuiu.MarinenkovaKD.Sprint2.Task0.V11.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MarinenkovaKD.Sprint2.Task0.V11.Lib
+{
+    public class DataService : ISprint2Task0V11
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x - 7830 == y;
+            res[1] = x - 7830 != y;
+            res[2] = y < x;
+            res[3] = y > x;
+            res[4] = y <= x;
+            res[5] = y >= x;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MarinenkovaKD.Sprint2.Task0.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MarinenkovaKD.Sprint2.Task0.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MarinenkovaKD.Sprint2.Task0.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c88f6f1-cdd0-464e-aca7-0a4319e74624")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
